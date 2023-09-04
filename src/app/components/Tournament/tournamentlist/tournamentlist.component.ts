64fc09ed8d62fb54ad0f350764db4246a739447c
emmanueldorantes <emmanueldorantesayala1984@gmail.com>
2023-09-03 22:42:40 -0600

    Formularios

---
+++ src/app/components/Tournament/tournamentlist/tournamentlist.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { TitleService } from '../../../services/title.service';
+
+@Component({
+  selector: 'app-tournamentlist',
+  templateUrl: './tournamentlist.component.html',
+  styleUrls: ['./tournamentlist.component.css']
+})
+export class TournamentlistComponent {
+  
+  constructor(private titleService: TitleService) {}
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Torneo / Lista de Torneo');
+  }
+}
